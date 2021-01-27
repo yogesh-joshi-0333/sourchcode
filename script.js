807151ed2121a9245f834e39270600c408a32aff
Joshi yogesh <joshiyogesh0333@gmail.com>
2021-01-27 17:25:25 +0530

    javascript file
    create new file for only js script

---
+++ script.js
@@ -0,0 +1,374 @@
+
+// DEVELOPER UNABLE TO CHECK CODE JS
+
+$(document).keydown(function (event) {
+if (event.keyCode == 123) { 
+return false;
+} else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
+return false;
+}
+});								
+$(document).ready(function() {
+
+$(document)[0].oncontextmenu = function() { return false; }
+
+$(document).mousedown(function(e) {
+if( e.button == 2 ) {
+alert('Sorry, this functionality is disabled!');
+return false;
+} else {
+return true;
+}
+});
+});
+
+
+
+
+// Google address search 
+
+        google_place_search(document.getElementById('origin'));
+        google_place_search(document.getElementById('destination'));
+        
+        function google_place_search(input)
+        {
+            var autocomplete = new google.maps.places.Autocomplete(input);
+            google.maps.event.addListener(autocomplete, 'place_changed',   function () {
+            
+                var place = autocomplete.getPlace();
+                var lat = place.geometry.location.lat();
+                var long = place.geometry.location.lng();
+                console.log(lat + ", " + long);
+            });
+        }
+
+
+
+
+
+
+
+
+/* Ajax form submission with image */
+
+
+jQuery("form").submit(function(e)    {
+e.preventDefault();
+e.stopImmediatePropagation();
+	e.stopPropagation();	
+
+
+        var formdata = new FormData();
+        var img = jQuery('input[name="channel_img"]')[0].files[0];
+        var title = jQuery('#channel_name').val();
+        var cat = jQuery('#cat').val();
+        var detail = jQuery('#channel_detail').val();
+        
+        formdata.append("img", img);
+        formdata.append("title", title);
+        formdata.append("cat", cat);
+        formdata.append("detail", detail);
+        formdata.append("action", 'create_cst_post');
+        formdata.append("type", 'wpstream_product');
+        
+          jQuery.ajax({
+            type: 'POST',
+            url: '<?=admin_url("admin-ajax.php")?>',
+            traditional: true,
+            processData: false,
+	contentType: false,
+            data:formdata,
+            success: function (response)
+	{
+		post_id = response.post_id;
+ console.log(response);
+            },
+            error: function (jqXHR,textStatus,errorThrown) {
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+             
+            }
+        });
+});
+
+
+/*  element view on screen event */
+
+jQuery(window).scroll(function() {
+    var top_of_element = jQuery("#video-8186-1_html5").offset().top;
+    var bottom_of_element = jQuery("#video-8186-1_html5").offset().top + jQuery("#video-8186-1_html5").outerHeight();
+    var bottom_of_screen = jQuery(window).scrollTop() + jQuery(window).innerHeight();
+    var top_of_screen = jQuery(window).scrollTop();
+
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+        jQuery('#video-8186-1_html5').trigger('play');
+            var video = jQuery('#video-8186-1_html5').get(0);
+            if ( video.paused ) {               jQuery('#video-8186-1_html5').trigger('play');
+            } 
+    } else {
+       
+    }
+});
+
+/*  remove emoji code javascript*/
+const line_text = jQuery(this).val();
+
+  resp = Array.from(line_text, x => {let theUnicode = x.charCodeAt(0).toString(16); while (theUnicode.length < 4) {theUnicode = '0' + theUnicode; } if (theUnicode < '00ff') {return x;}}).join('');
+
+
+jQuery(this).val(resp);
+
+
+Sending mail with javascript code
+
+$.ajax({
+ type: “POST”,
+ url: “https://mandrillapp.com/api/1.0/messages/send.json”,
+ data: {
+   ‘key’: ‘YOUR API KEY HERE’,
+   ‘message’: {
+     ‘from_email’: ‘YOUR@EMAIL.HERE’,
+     ‘to’: [
+         {
+           ‘email’: ‘RECIPIENT_NO_1@EMAIL.HERE’,
+           ‘name’: ‘RECIPIENT NAME (OPTIONAL)’,
+           ‘type’: ‘to’
+         },
+         {
+           ‘email’: ‘RECIPIENT_NO_2@EMAIL.HERE’,
+           ‘name’: ‘ANOTHER RECIPIENT NAME (OPTIONAL)’,
+           ‘type’: ‘to’
+         }
+       ],
+     ‘autotext’: ‘true’,
+     ‘subject’: ‘YOUR SUBJECT HERE!’,
+     ‘html’: ‘YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!’
+   }
+ }
+}).done(function(response) {
+  console.log(response); // if you're into that sorta thing
+});
+==============================================
+
+
+/**************** for scrolling UP and DOWN ******************/
+
+
+var lastScrollTop = 0;
+    $(window).on('scroll', function() {
+        st = $(this).scrollTop();
+        if(st < lastScrollTop) {
+            console.log('up 1');
+        }
+        else {
+            console.log('down 1');
+        }
+        lastScrollTop = st;
+    })
+
+/************** APPLY CSS IN IFRAME ****************/
+
+$('#id').load(function(){
+$("#id").contents().find("#buddypress").css("margin-top", "-100px");
+});
+
+
+/********* Uploading files and images with AJAX ***********/
+$('#bac-img').change(function(){
+if (this.files && this.files[0] ){				    	
+		        var reader = new FileReader();
+		        //var p_id = jQuery("#post_ID").val();
+		        reader.readAsDataURL(this.files[0]);
+		    }
+		    var file_data = jQuery('#bac-img').prop('files')[0];  
+			var form_data = new FormData();                  
+			form_data.append('bacimg', file_data); 
+			$.ajax({
+			url: '../wp-content/themes/Divi/custom_calender/image-upload-ajax.php', 
+			dataType: 'text',  
+			cache: false,
+			contentType: false,
+			processData: false,
+			data: form_data,                         
+			type: 'post',
+			success: function(response){
+					}	});
+		      });
+          
+          /****** upload image PROGRESS BAR ******/
+/*** adding this in ajax code before success ******/
+xhr: function() {
+          var myXhr = jQuery.ajaxSettings.xhr();
+           if(myXhr.upload){
+                       myXhr.upload.addEventListener('progress',progress, false);
+                    }
+              return myXhr;
+        },
+success: function(response){
+	 jQuery("#progBar").hide();
+}
+
+
+function progress(e){
+        if(e.lengthComputable){
+            jQuery('progress').attr({value:e.loaded,max:e.total});
+            var percentage = (e.loaded / e.total) * 100;
+            jQuery('#prog').html(percentage.toFixed(0)+'%');
+        }
+    }
+ 
+    function resetProgressBar() {
+        jQuery('#prog').html('0%');
+        jQuery('progress').attr({value:0,max:100});
+    }
+/**********************************End Code*************************************************/
+
+
+/******************************check div content size**********************/
+<script>
+$.fn.textWidth = function(text, font) {
+    if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
+    $.fn.textWidth.fakeEl.text(text || this.val() || this.text()).css('font', font || this.css('font'));
+    return $.fn.textWidth.fakeEl.width();
+};
+ 
+$('.text-preview').on('keypress', function(event) {
+  if(event.keyCode==13){
+   		event.preventDefault();
+  }
+  
+  
+  if($(this).textWidth() > $('.text-preview').width()-30 ){
+    event.preventDefault();
+  }
+}).trigger('input');
+</script>
+
+/******************************end check div content size**********************/
+
+/******************************Get Exact mouse position in canvas ******************************/
+function getRelativeMousePosition(event, target) {
+			target = target || event.target;
+			var rect = target.getBoundingClientRect();
+
+			return {
+			    x: event.clientX - rect.left,
+			    y: event.clientY - rect.top,
+			}
+		}
+
+		// assumes target or event.target is canvas
+function getNoPaddingNoBorderCanvasRelativeMousePosition(event, target) {
+			target = target || event.target;
+			var pos = getRelativeMousePosition(event, target);
+
+			pos.x = pos.x * target.width  / target.clientWidth;
+			pos.y = pos.y * target.height / target.clientHeight;
+
+			return pos;  
+		}
+       	const pos = getNoPaddingNoBorderCanvasRelativeMousePosition(e, canvas);
+
+  		mouse_at.x = pos.x;
+  		mouse_at.y = pos.y;
+
+
+/******************************End code ******************************/
+/******************* Admin Ajax wordpress *************************/
+var url = "<?php echo admin_url('admin-ajax.php'); ?>";
+
+    jQuery.ajax({
+
+           type: 'post',
+           url: url,
+           data: {action:'action_name',form_data:jQuery("#idForm").serialize()},
+           success: function(data)           {	
+
+           }
+         });
+
+function function_name(){
+
+	die();
+
+}
+
+
+/********************************* Set & destroy cookie in jQuery **********************************/
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
+<script type="text/javascript">
+	$("#add").click(function(){
+		$.cookie('login-vault', '1');
+	});
+	$("#delete").click(function(){
+		 var date = new Date();
+         var m = 10;
+		 date.setTime(date.getTime() - (m * 60 * 1000));
+		 $.cookie('login-vault', '1', { expires: date });
+	});
+/******************************** End cookie in jQuery ************************************/
+/**************** for scrolling UP and DOWN ******************/
+
+
+var lastScrollTop = 0;
+    $(window).on('scroll', function() {
+        st = $(this).scrollTop();
+        if(st < lastScrollTop) {
+            console.log('up 1');
+        }
+        else {
+            console.log('down 1');
+        }
+        lastScrollTop = st;
+    })
+
+/************** APPLY CSS IN IFRAME ****************/
+
+$('#id').load(function(){
+$("#id").contents().find("#buddypress").css("margin-top", "-100px");
+});
+/********* Uploading files and images with AJAX ***********/
+$('#bac-img').change(function(){
+if (this.files && this.files[0] ){				    	
+		        var reader = new FileReader();
+		        //var p_id = jQuery("#post_ID").val();
+		        reader.readAsDataURL(this.files[0]);
+		    }
+		    var file_data = jQuery('#bac-img').prop('files')[0];  
+			var form_data = new FormData();                  
+			form_data.append('bacimg', file_data); 
+			$.ajax({
+			url: '../wp-content/themes/Divi/custom_calender/image-upload-ajax.php', 
+			dataType: 'text',  
+			cache: false,
+			contentType: false,
+			processData: false,
+			data: form_data,                         
+			type: 'post',
+			success: function(response){
+					}	});
+		      });
+          
+          
+/******************************check div content size**********************/
+<script>
+$.fn.textWidth = function(text, font) {
+    if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
+    $.fn.textWidth.fakeEl.text(text || this.val() || this.text()).css('font', font || this.css('font'));
+    return $.fn.textWidth.fakeEl.width();
+};
+ 
+$('.text-preview').on('keypress', function(event) {
+  if(event.keyCode==13){
+   		event.preventDefault();
+  }
+  
+  
+  if($(this).textWidth() > $('.text-preview').width()-30 ){
+    event.preventDefault();
+  }
+}).trigger('input');
+</script>
+
+/******************************end check div content size**********************/
