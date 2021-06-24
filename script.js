cb21a5e4603f28a262ee1247009a130117a57208
Joshi yogesh <joshiyogesh0333@gmail.com>
2021-06-24 15:47:49 +0530

    copy text on click function

--- script.js
+++ script.js
@@ -519,3 +519,49 @@ warning.toString = function() {
 
 /****************************** END  Console warning Code **********************/
 console.log('%c', warning);
+
+
+
+
+/***************** copy text on click function *********************/
+
+ function copyToClipboard() {
+    var success = true,
+      range = document.createRange(),
+      selection;
+
+    // For IE.
+    if (window.clipboardData) {
+      window.clipboardData.setData("Text", "<?php echo home_url( $wp->request ); ?>");
+    } else {
+      // Create a temporary element off screen.
+      var tmpElem = $('<div>');
+      tmpElem.css({
+        position: "absolute",
+        left: "-1000px",
+        top: "-1000px",
+      });
+      // Add the input value to the temp element.
+      tmpElem.text("<?php echo home_url( $wp->request ); ?>");
+      $("body").append(tmpElem);
+      // Select temp element.
+      range.selectNodeContents(tmpElem.get(0));
+      selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+      // Lets copy.
+      try {
+        success = document.execCommand("copy", false, null);
+      }
+      catch (e) {
+        copyToClipboardFF(input.val());
+      }
+      if (success) {
+        alert("Shareble Link Copy");
+        // remove temp element.
+        tmpElem.remove();
+      }
+    }
+  }
+ 
+/***************** END copy text on click function *********************/
