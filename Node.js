9c6ab8710c46a886ae66517bfbf29ea0764bbd7c
Joshi yogesh <joshiyogesh0333@gmail.com>
2021-07-16 15:41:43 +0530

    Unrestrict to exicution in Windows

---
+++ Node.js
@@ -0,0 +1,6 @@
+/* unlock script Exicution or Unrestrict to exicution  in Windows */
+/* Run this command in cmd with root folder of your project which have packge.json file */
+
+  Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+
+/* END  */
