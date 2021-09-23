42d740c47074a3f6f10d972eb26e7dcf391fbf7b
Joshi yogesh <joshiyogesh0333@gmail.com>
2021-09-23 11:35:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+/* check element in Viewport function  */
+
+function elementInViewport2(el) {
+	var top = el.offsetTop;
+	var left = el.offsetLeft;
+	var width = el.offsetWidth;
+	var height = el.offsetHeight;
+  
+	while(el.offsetParent) {
+	  el = el.offsetParent;
+	  top += el.offsetTop;
+	  left += el.offsetLeft;
+	}
+  	return (
+	  top < (window.pageYOffset + window.innerHeight) &&
+	  left < (window.pageXOffset + window.innerWidth) &&
+	  (top + height) > window.pageYOffset &&
+	  (left + width) > window.pageXOffset
+	);
+  }
+/* code End */
+
 /* START windows after load javascript */
 
   window.addEventListener("load", function(){});
