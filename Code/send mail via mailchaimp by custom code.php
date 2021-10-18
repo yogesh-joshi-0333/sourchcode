d4ee0662480406a6c262533d4ab663f725d5121f
Joshi yogesh <joshiyogesh0333@gmail.com>
2021-10-18 11:35:30 +0530

    Sample Code for Mailchaip API

--- Code/send mail via mailchaimp by custom code.php
+++ Code/send mail via mailchaimp by custom code.php
@@ -1,19 +1,16 @@
-function form_mailke()
+function test_mail()
 {
 	$email = $_POST['emails'];
-	$list_id = 'b2bd952c27';
-	$api_key = 'c03addccd39e2a0ad5d5572917bef6b9-us17';
+	$id = 'fsdgfdsgsdg';
+	$api = 'dsbfjdsd654fhsdfhdslkfdgfhjsg';
 	$data_center = substr($api_key,strpos($api_key,'-')+1);	
-	$url = 'https://'. $data_center .'.api.mailchimp.com/3.0/lists/'. $list_id .'/members';
+	$url = 'https://'. $data_center .'.api.mailchimp.com/3.0/lists/'. $id .'/user';
 	
-	$json = json_encode([
-		'email_address' => $email,
-		'status'        => 'subscribed', //pass 'subscribed' or 'pending'
-	]);
+	$json = json_encode(['email_address' => $email,'status'=> 'user', //pass 'subscribed' or 'pending']);
 	
 	try {
 		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $api_key);
+		curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $api);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
@@ -26,15 +23,16 @@ function form_mailke()
 	
 		if (200 == $status_code) {
 			
-			$myrows = 'Thank you for subscribing!';
+			$message = 'Thank you';
 		}
 		else if (400 == $status_code) 
 		{
-			$myrows = 'Member Exists';
+			$message = 'User Exists';
 		}
-	} catch(Exception $e) {
-		$myrows = $e->getMessage();
-		
+	} 
+	catch(Exception $e) 
+	{
+		$message = $e->getMessage();
 	}
-		$a=wp_send_json_success($myrows);
+		$a=wp_send_json_success($message);
 }
